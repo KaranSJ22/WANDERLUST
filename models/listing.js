99ea10b76e3baae535975773791969ec78c980e6
KaranSJ22 <karanjamkhandi@gmail.com>
2024-06-28 00:55:52 +0530

    first commit

---
+++ models/listing.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+
+const listingSchema= new Schema({
+    title:{ 
+        type:String,
+        required:true,
+    },
+    description:String,
+    image:{
+        filename: String,
+        url:{
+            type:String,
+            default:"https://tinyurl.com/uc37ux9c",
+            set: (v) => v==="" ? "https://tinyurl.com/uc37ux9c" : v,
+        }
+    },
+    price:Number,
+    location:String,
+    country:String,
+});
+
+const Listing= mongoose.model("Listing",listingSchema);
+module.exports=Listing; 
