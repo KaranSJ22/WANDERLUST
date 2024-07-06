1c5a319045372e6f6d76e1036f4ca9b63206a7bc
KaranSJ22 <karanjamkhandi@gmail.com>
2024-07-06 18:30:38 +0530

    initial commit

--- app.js
+++ app.js
@@ -108,7 +108,8 @@ app.all("*",(req,res,next)=>{
 
 app.use((err, req, res, next) => {
    let {statusCode=500 , message="somethin went wrong"}=err;
-    res.status(statusCode).send(message);
+   res.render("error.ejs",{statusCode,message});
+    // res.status(statusCode).send(message);
 });
 
 app.listen(8080,()=>{
