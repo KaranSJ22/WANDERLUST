ecbebd6c611bf3829d486aa5f3ef50866a9f3bdf
KaranSJ22 <karanjamkhandi@gmail.com>
2024-08-25 16:09:23 +0530

    Initial Commit

--- models/review.js
+++ models/review.js
@@ -1,2 +1,17 @@
 const mongoose=require("mongoose");
 const Schema=mongoose.Schema;
+
+const reviewSchema= new Schema({
+    comment:{
+        type:String,
+        required:true,
+    },
+    rating:{
+        type:Number,
+        required:true,
+    },
+    createdAt:Date,
+});
+
+const Review= mongoose.model("Review",reviewSchema);
+module.exports=Review; 
